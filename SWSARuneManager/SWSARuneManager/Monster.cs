41319e081da92420f9571a1c14cfb09b1435e317
xuanyou <xuanyou@gmail.com>
2015-05-13 01:50:15 +0800

    Created Monster and Rune classes and Unit Tests
    Created Monster and Rune classes and Unit Tests

---
+++ SWSARuneManager/SWSARuneManager/Monster.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWSARuneManager
+{
+    [Serializable]
+    public class Monster
+    {
+        public string Name { get; set; }
+        public int HitPoints { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int Speed { get; set; }
+        public int CriticalRate { get; set; }
+        public int CriticalDamage { get; set; }
+        public int Resistance { get; set; }
+        public int Accuracy { get; set; }
+
+        private void InitDefaultValues()
+        {
+            Name = "";
+            HitPoints = 0;
+            Attack = 0;
+            Defense = 0;
+            Speed = 0;
+            CriticalRate = 15;
+            CriticalDamage = 50;
+            Resistance = 15;
+            Accuracy = 0;
+        }
+
+        public Monster()
+        {
+            InitDefaultValues();
+        }
+        
+    }
+
+    public class Rune
+    {
+        public enum RuneType
+        {
+            Energy,
+            Blade,
+            Swift,
+            Rage,
+            Vampire,
+            Guard,
+            Will,
+            Revenge,
+            Despair,
+            Violent,
+            Focus,
+            Fatal,
+            Endure,
+            Nemesis,
+            Shield
+        }
+
+        private void InitDefaultValues(int position=0, int level=0)
+        {
+
+            Position = 1;
+            Level = 0;
+
+            HitPointsPercent = 0;
+            HitPoints = 0;
+            AttackPercent = 0;
+            Attack = 0;
+            DefensePercent = 0;
+            Defense = 0;
+            Speed = 0;
+            CriticalRate = 0;
+            CriticalDamage = 0;
+            Resistance = 0;
+            Accuracy = 0;
+
+            HasFakeStat = false;
+        }
+
+        public RuneType Type { get; set; }
+        public int Position { get; set; }
+        public int Level { get; set; }
+        
+        public int HitPointsPercent { get; set; }
+        public int HitPoints { get; set; }
+        public int AttackPercent { get; set; }
+        public int Attack { get; set; }
+        public int DefensePercent { get; set; }
+        public int Defense { get; set; }
+        public int Speed { get; set; }
+        public int CriticalRate { get; set; }
+        public int CriticalDamage { get; set; }
+        public int Resistance { get; set; }
+        public int Accuracy { get; set; }
+
+        public Boolean HasFakeStat;
+
+        private int CountSubStats()
+        {
+            int count = 0;
+            if (HitPointsPercent > 0) count++;
+            if (HitPoints > 0) count++;
+            if (AttackPercent > 0) count++;
+            if (Attack > 0) count++;
+            if (DefensePercent > 0) count++;
+            if (Defense > 0) count++;
+            if (Speed > 0) count++;
+            if (CriticalRate > 0) count++;
+            if (CriticalDamage > 0) count++;
+            if (Resistance > 0) count++;
+            if (Accuracy > 0) count++;
+
+            // Subtract 1 for the main stat
+            count--;
+
+            // Subtract one more if there is a fake stat
+            if (HasFakeStat) count--;
+
+            return count;
+        }
+
+        public Boolean IsMaxed()
+        {
+            Boolean returnValue = false;
+
+            if (Position == 2 || Position == 4 || Position == 6)
+            {
+                // Main Stat Runes (2,4,6)
+                if (Level == 15) returnValue = true;
+            }
+            else
+            {
+                // Substat Runes (1,3,5)
+                int numSubStats = CountSubStats();
+                if (Level >= (numSubStats * 3)) returnValue = true;
+            }
+
+            return returnValue;
+        }
+    }
+}
+
---
+++ SWSARuneTest/SWSARuneTest/UnitTests.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SWSARuneManager;
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Collections.Generic;
+
+namespace SWSARuneTest
+{
+    [TestClass]
+    public class MonsterTest
+    {
+        private Monster GenerateTestMonster(string name)
+        {
+            Monster monster = new Monster();
+
+            if (name.Equals("Shannon", StringComparison.CurrentCultureIgnoreCase))
+            {
+                monster.Name = "Shannon";
+                monster.HitPoints = 7245;
+                monster.Attack = 602;
+                monster.Defense = 560;
+                monster.Speed = 111;
+            }
+            else if (name.Equals("Bernard", StringComparison.CurrentCultureIgnoreCase))
+            {
+                monster.Name = "Bernard";
+                monster.HitPoints = 10377;
+                monster.Attack = 418;
+                monster.Defense = 703;
+                monster.Speed = 111;
+            }
+
+            return monster;
+        }
+
+        private Boolean MonsterPropertiesEqual(Monster a, Monster b)
+        {
+            if (ReferenceEquals(a, b)) return true;
+            
+            Boolean isEqual = true;
+            isEqual &= a.Name.Equals(b.Name, StringComparison.CurrentCultureIgnoreCase);
+            isEqual &= a.HitPoints.Equals(b.HitPoints);
+            isEqual &= a.Attack.Equals(b.Attack);
+            isEqual &= a.Defense.Equals(b.Defense);
+            isEqual &= a.Speed.Equals(b.Speed);
+            isEqual &= a.CriticalRate.Equals(b.CriticalRate);
+            isEqual &= a.CriticalDamage.Equals(b.CriticalDamage);
+            isEqual &= a.Resistance.Equals(b.Resistance);
+            isEqual &= a.Accuracy.Equals(b.Accuracy);
+
+            return isEqual;
+        }
+
+        [TestMethod]
+        public void TestSerialization()
+        {
+            Monster monster1 = GenerateTestMonster("Shannon");
+            
+            IFormatter formatter = new BinaryFormatter();
+            MemoryStream memoryStream = new MemoryStream();
+            formatter.Serialize(memoryStream, monster1);
+
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            Monster monster2 = (Monster) formatter.Deserialize(memoryStream);
+            Assert.AreEqual(monster1.Name, monster2.Name);
+            Assert.AreEqual(monster1.HitPoints, monster2.HitPoints);
+        }
+
+        [TestMethod]
+        public void TestListSerialization()
+        {
+            Monster monster1 = GenerateTestMonster("Shannon");
+            Monster monster2 = GenerateTestMonster("Bernard");
+
+            List<Monster> list = new List<Monster>();
+            list.Add(monster1);
+            list.Add(monster2);
+
+            IFormatter formatter = new BinaryFormatter();
+            MemoryStream memoryStream = new MemoryStream();
+            formatter.Serialize(memoryStream, list);
+
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            List<Monster> readList = (List<Monster>)formatter.Deserialize(memoryStream);
+
+            int i = 0;
+            foreach (Monster m in readList)
+            {
+                Assert.IsTrue(MonsterPropertiesEqual(m, list[i]));
+                i++;
+            }
+        }
+
+        [TestMethod]
+        public void TestEquals()
+        {
+            Monster monster1 = GenerateTestMonster("Shannon");
+            Monster monster2 = GenerateTestMonster("Shannon");
+            Assert.IsTrue(MonsterPropertiesEqual(monster1,monster2));
+        }
+    }
+
+    [TestClass]
+    public class RuneTest
+    {
+        [TestMethod]
+        public void TestRuneMax()
+        {
+            Rune rune = new Rune();
+            rune.Position = 1;
+            rune.Attack = 1;
+            rune.CriticalDamage = 5;
+            rune.Level = 3;
+            Assert.IsTrue(rune.IsMaxed());
+
+            // If the rune has one more stat, then it's not maxed.
+            rune.HitPoints = 5;
+            Assert.IsFalse(rune.IsMaxed());
+
+            // If that one more stat is fake, then it's maxed.
+            rune.HasFakeStat = true;
+            Assert.IsTrue(rune.IsMaxed());
+            
+            // Again, if the rune has one more stat, it's not maxed.
+            rune.Speed = 5;
+            Assert.IsFalse(rune.IsMaxed());
+
+            // If the rune is now level 6, then it's maxed.
+            rune.Level = 6;
+            Assert.IsTrue(rune.IsMaxed());
+        }
+    }
+
+}
